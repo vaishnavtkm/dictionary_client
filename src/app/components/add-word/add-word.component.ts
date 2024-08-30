4f5fc97e81f7fe5363f1b35aa77c23a9194cdb31
vaishnavtkm <vaith@eg.dk>
2024-08-30 17:36:28 +0530

    homepage done

--- src/app/components/add-word/add-word.component.ts
+++ src/app/components/add-word/add-word.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 @Component({
   selector: 'app-add-word',
   standalone: true,
-  imports: [],
+  imports: [CalendarModule,FormsModule],
   templateUrl: './add-word.component.html',
-  styleUrl: './add-word.component.scss'
+  styleUrl: './add-word.component.scss',
 })
 export class AddWordComponent {
-
 }
