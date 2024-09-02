1bd776a6056f038694b0bebec881bd55bf9f0fc8
vaishnavtkm <vaith@eg.dk>
2024-09-02 18:01:08 +0530

    getAll operation WD

--- src/app/components/allwords/allwords.component.ts
+++ src/app/components/allwords/allwords.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { wordResponse } from '../../Models/wordResponse';
+import { DictionaryServiceService } from '../../services/dictionary-service.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-allwords',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './allwords.component.html',
-  styleUrl: './allwords.component.scss'
+  styleUrl: './allwords.component.scss',
 })
-export class AllwordsComponent {
+export class AllwordsComponent implements OnInit {
+  dictionaryWords$!: Observable<wordResponse[]>;
 
+  constructor(private dictionaryService: DictionaryServiceService) {}
+  ngOnInit(): void {
+    this.dictionaryWords$ = this.dictionaryService.getAllWords();
+  }
 }
