995d6af5190a03277e1a08da1969c89759f27d88
vaishnavtkm <vaith@eg.dk>
2024-09-02 13:12:31 +0530

    theme done

--- src/app/components/add-word/add-word.component.ts
+++ src/app/components/add-word/add-word.component.ts
@@ -1,21 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ColorPickerModule } from 'primeng/colorpicker';
 import { CalendarModule } from 'primeng/calendar';
 import { DictionaryServiceService } from '../../services/dictionary-service.service';
 import { wordRequest } from '../../Models/wordRequest';
 
-
 @Component({
   selector: 'app-add-word',
   standalone: true,
-  imports: [FormsModule,ColorPickerModule,CommonModule,CalendarModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    ColorPickerModule,
+    CommonModule,
+    CalendarModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './add-word.component.html',
   styleUrl: './add-word.component.scss',
 })
 export class AddWordComponent implements OnInit {
-  constructor(private SendService: DictionaryServiceService) { }
+  constructor(private SendService: DictionaryServiceService) {}
 
   Danish_word = new FormControl('', Validators.required);
   Pronounciation = new FormControl('', Validators.required);
@@ -28,31 +39,11 @@ export class AddWordComponent implements OnInit {
     Pronounciation: this.Pronounciation,
     English_word: this.English_word,
     Meaning: this.Meaning,
-    date: this.date
+    date: this.date,
   });
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onPost()
-  {
-    if(this.PostForm.valid)
-    {
-      const fromData: wordRequest = {
-        date: this.PostForm.value.date,
-        Dansih_word: this.PostForm.value.Danish_word,
-        Pronounciation: this.PostForm.value.Pronounciation,
-        English_word: this.PostForm.value.English_word,
-        Meaning: this.PostForm.value.Meaning
-      }
-      console.log(this.PostForm.value);
-      this.SendService.addWord(fromData).subscribe((data) => {
-        console.log(data);
-      });
-    }
+  onPost() {
+   
   }
-
 }
-
-
-
-
