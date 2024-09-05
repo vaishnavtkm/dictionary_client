1588337b40d27dec9401eb2a1d8e87cbb5dac43f
vaishnavtkm <vaith@eg.dk>
2024-09-05 11:23:07 +0530

    loading screen added WD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { SinglewordComponent } from './components/singleword/singleword.component';
 import { AddWordComponent } from './components/add-word/add-word.component';
 import { CalendarModule } from 'primeng/calendar';
-import { LineanimeComponent } from "./lineanime/lineanime.component";
+import { LineanimeComponent } from './lineanime/lineanime.component';
 import { CustomCursorComponent } from './components/custom-cursor/custom-cursor.component';
+import { InitializationService } from './services/initialization.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HomepageComponent, 
-    SinglewordComponent, 
-    AddWordComponent, 
-    CalendarModule, LineanimeComponent,
+    HomepageComponent,
+    SinglewordComponent,
+    AddWordComponent,
+    CalendarModule,
+    LineanimeComponent,
     CustomCursorComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dictionary_client';
+
+  constructor(private initializationService: InitializationService) {}
+  isLoading = true;
+
+  ngOnInit(): void {
+    this.initializationService.initializeApp().subscribe(() => {
+      this.isLoading = false;
+    });
+  }
 }
---
+++ src/app/services/initialization.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InitializationService {
+  initializeApp(): Observable<any> {
+    return of(true).pipe(delay(3000));
+  }
+  constructor() {}
+}
